4d4d1966036591292e60636302d0daaf90b69bd6
Matheus Pecinalli <pecinallipoker@gmail.com>
2023-06-04 18:14:41 -0300

    Modal em produção

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -4,14 +4,15 @@ import useFetch from '../../Hooks/useFetch';
 import { PHOTOS_GET } from '../../Api';
 import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
+import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = () => {
+const FeedPhotos = ({ setModalPhoto }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
       const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-      const { response, json } = await request(url, options);
+      const { json } = await request(url, options);
       console.log(json);
     }
     fetchPhotos();
@@ -21,9 +22,15 @@ const FeedPhotos = () => {
   if (loading) return <Loading />;
   if (data)
     return (
-      <div>
-        <FeedPhotosItem />
-      </div>
+      <ul className={`${styles.feed} animeLeft`}>
+        {data.map((photo) => (
+          <FeedPhotosItem
+            key={photo.id}
+            photo={photo}
+            setModalPhoto={setModalPhoto}
+          />
+        ))}
+      </ul>
     );
   else return null;
 };
--- src/Components/Feed/FeedPhotosItem.jsx
+++ src/Components/Feed/FeedPhotosItem.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import styles from './FeedPhotosItem.module.css';
 
-const FeedPhotosItem = () => {
-  return <div>FeedPhotosItem</div>;
+const FeedPhotosItem = ({ photo, setModalPhoto }) => {
+  function handleClick() {
+    setModalPhoto(photo);
+  }
+
+  return (
+    <li className={styles.photo} onClick={handleClick}>
+      <img src={photo.src} alt={photo.title} />
+      <span className={styles.visualizacao}>{photo.acessos}</span>
+    </li>
+  );
 };
 
 export default FeedPhotosItem;
