9b0558aa2207bc3e6350c2b00ed5f433562cf126
Matheus Pecinalli <pecinallipoker@gmail.com>
2023-06-05 18:28:09 -0300

    Adicionado a funçao de comentar e deletar a foto, e animaçao de a foto e animaçao de loanding

--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -2,24 +2,32 @@ import React from 'react';
 import styles from './PhotoContent.module.css';
 import { Link } from 'react-router-dom';
 import PhotoComments from './PhotoComments';
+import { UserContext } from '../../UserContext';
+import PhotoDelete from './PhotoDelete';
+import Image from '../Helper/Image';
 
 const PhotoContent = ({ data }) => {
+  const user = React.useContext(UserContext);
   const { photo, comments } = data;
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div>
-          <p>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
-            <span className={styles.visualizaçoes}>{photo.acessos}</span>
+          <p className={styles.author}>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
+            <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
           <h1 className="title">
             <Link to={`/photo/${photo.id}`}>{photo.title}</Link>
           </h1>
-          <ul className={styles.atributes}>
+          <ul className={styles.attributes}>
             <li>{photo.peso} kg</li>
             <li>{photo.idade} anos</li>
           </ul>
